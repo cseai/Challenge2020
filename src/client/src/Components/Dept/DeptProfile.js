fb2e88cc57a5266347d1fdee9e0146706e8cef68
belal-bh <bh.pro.pust@gmail.com>
2020-07-01 20:02:14 +0600

    DeptProfile updated

--- src/client/src/Components/Dept/DeptProfile.js
+++ src/client/src/Components/Dept/DeptProfile.js
@@ -41,6 +41,7 @@ const Dept = ({ getDept, isAuthenticated, match: { params }, dept: { loading, de
 	const [info, setInfo] = useState(false);
 	const [contact, setContact] = useState(false);
 	const [location, setLocation] = useState(false);
+	const [deptMap, setDeptMap] = useState(false);
 
 	//eduhub loading before page show
 	useEffect(() => {
@@ -181,79 +182,87 @@ const Dept = ({ getDept, isAuthenticated, match: { params }, dept: { loading, de
 							<DeptProfileInfoContentIcon
 								className={Styles.eduHub__profile__about_heading}
 								style={{ width: '155px' }}
+								onClick={() => setDeptMap(!deptMap)}
 							>
 								<div className={Styles.eduHub__profile__info_content_side}></div>
 								<p>Dept Map</p>
 							</DeptProfileInfoContentIcon>
-							{/* EduHub */}
-							{(dept.eduHub === null && dept.parent === null) ? (
-								// This is EduHub
+							{deptMap && (
 								<Fragment>
-									<div className={Styles.eduHub__profile__about_about_details}>This is a EduHub</div>
-								</Fragment>
-							):(
-								<Fragment>
-									<div className={Styles.eduHub__profile__about_about_details}><h3>Child Departments:</h3></div>
-									<div className={Styles.eduHub__profile__about_about_details}>
-									<Link to={`/dept/${dept.eduHub.username}`}>
-										<DeptProfileInfoContentIcon
-											key={dept.eduHub.username}
-											className={Styles.eduHub__profile__eduhubmap}
-										>
-											<DeptMap className={Styles.eduHub__profile__eduhubmap_p}>
-												{dept.eduHub.name}
-											</DeptMap>
-										</DeptProfileInfoContentIcon>
-									</Link>
-									<Link to={`/dept/${dept.parent.username}`}>
-										<DeptProfileInfoContentIcon
-											key={dept.parent.username}
-											className={Styles.eduHub__profile__eduhubmap}
-										>
-											<DeptMap className={Styles.eduHub__profile__eduhubmap_p}>
-												{dept.parent.name}
-											</DeptMap>
-										</DeptProfileInfoContentIcon>
-									</Link>
-									</div>
-								</Fragment>
-							)}
-							{/* Parent */}
-
-
-							{/* Children */}
-							{dept.children.length === null ? (
-								<Fragment>
-									<div
-										style={{
-											display: 'flex',
-											justifyContent: 'center',
-										}}
-									>
-										<img
-											src={Spinner}
-											alt='sub dept loading'
-											style={{ height: '40px', width: '40px' }}
-										/>
-									</div>
-								</Fragment>
-							) : (
-								<Fragment>
-									<div className={Styles.eduHub__profile__about_about_details}><h3>Child Departments:</h3></div>
-									<div className={Styles.eduHub__profile__about_about_details}>
-										{dept.children.map((dept, index) => (
-											<Link to={`/dept/${dept.username}`}>
+									{/* EduHub and Parent*/}
+									{(dept.eduHub === null && dept.parent === null) ? (
+										// This is EduHub
+										<Fragment>
+											<div className={Styles.eduHub__profile__about_about_details}>
+											<div><p>Showing map from EduHub</p></div>
+											</div>
+										</Fragment>
+									):(
+										<Fragment>
+											{/* <div className={Styles.eduHub__profile__about_about_details}><h3>Super Departments:</h3></div> */}
+											<div className={Styles.eduHub__profile__about_about_details}>
+											<div><p>EduHub:</p></div>
+											<Link to={`/dept/${dept.eduHub.username}`}>
 												<DeptProfileInfoContentIcon
-													key={index}
+													key={dept.eduHub.username}
 													className={Styles.eduHub__profile__eduhubmap}
 												>
 													<DeptMap className={Styles.eduHub__profile__eduhubmap_p}>
-														{dept.name}
+														{dept.eduHub.name}
 													</DeptMap>
 												</DeptProfileInfoContentIcon>
 											</Link>
-										))}
-									</div>
+											<div><p>Parent Department:</p></div>
+											<Link to={`/dept/${dept.parent.username}`}>
+												<DeptProfileInfoContentIcon
+													key={dept.parent.username}
+													className={Styles.eduHub__profile__eduhubmap}
+												>
+													<DeptMap className={Styles.eduHub__profile__eduhubmap_p}>
+														{dept.parent.name}
+													</DeptMap>
+												</DeptProfileInfoContentIcon>
+											</Link>
+											</div>
+										</Fragment>
+									)}
+		
+									{/* Children */}
+									{dept.children.length < 1 ? (
+										<Fragment>
+											<div
+												style={{
+													display: 'flex',
+													justifyContent: 'center',
+												}}
+											>
+												<img
+													src={Spinner}
+													alt='sub dept loading'
+													style={{ height: '40px', width: '40px' }}
+												/>
+											</div>
+										</Fragment>
+									) : (
+										<Fragment>
+											{/* <div className={Styles.eduHub__profile__about_about_details}><h3>Child Departments:</h3></div> */}
+											<div className={Styles.eduHub__profile__about_about_details}>
+											<div><p>Child Departments:</p></div>
+												{dept.children.map((dept, index) => (
+													<Link to={`/dept/${dept.username}`}>
+														<DeptProfileInfoContentIcon
+															key={index}
+															className={Styles.eduHub__profile__eduhubmap}
+														>
+															<DeptMap className={Styles.eduHub__profile__eduhubmap_p}>
+																{dept.name}
+															</DeptMap>
+														</DeptProfileInfoContentIcon>
+													</Link>
+												))}
+											</div>
+										</Fragment>
+									)}
 								</Fragment>
 							)}
 						</DeptProfileAbout>
