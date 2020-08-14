68d04dd3cc75058af6ecc98bcf800f8e6d64dd95
belal-bh <bh.pro.pust@gmail.com>
2020-08-14 17:50:00 +0600

    Library Admin component added

---
+++ src/client/src/Components/Library/LibraryAdmin/LibraryAdmin.js
@@ -0,0 +1,20 @@
+import React, {Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// css and icon
+
+
+// components
+
+// action
+
+const LibraryAdmin = () => {
+    return (
+        <Fragment>
+            <hi>Library Admin Site</hi>
+        </Fragment>
+    );
+};
+
+export default LibraryAdmin;
